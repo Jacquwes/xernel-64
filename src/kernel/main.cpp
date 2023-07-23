109c5975839980759dd925e79bccc7b08b4f2490
Paul Hariel <paul.hariel@outlook.com>
2023-07-23 21:13:51 +0200

    Halt on program end

--- src/kernel/main.cpp
+++ src/kernel/main.cpp
@@ -1,4 +1,24 @@
+/*
+	xernel - x86_64 kernel written in C++20
+	Copyright (C) 2023  Jacquwes
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <limine.h>
+
+#include <stl/int.hpp>
 
 static volatile struct limine_framebuffer_request framebuffer_request =
 {
@@ -13,19 +33,19 @@ void halt()
 		asm("hlt");
 }
 
-void main()
+extern "C" void _start()
 {
-	if (framebuffer_request.response == NULL
+	if (framebuffer_request.response == 0
 		|| framebuffer_request.response->framebuffer_count < 1)
-		return;
+		halt();
 
 	limine_framebuffer *framebuffer = framebuffer_request.response->framebuffers[0];
 
 	for (size_t i = 0; i < 100; i++)
 	{
-		uint32_t* fb = framebuffer->address;
-		fb[i * (framebuffer->pitch / 4) + i] = 0xffffff;
+		uint32_t* fb = reinterpret_cast<uint32_t*>(framebuffer->address);
+		fb[i * (framebuffer->pitch / 4) + i] = 0x0000ff;
 	}
 
-	return;
+	halt();
 }
