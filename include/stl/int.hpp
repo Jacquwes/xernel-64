c651abd47d07cca2cde40316ea6130919dc4843a
Paul Hariel <paul.hariel@outlook.com>
2023-07-23 21:12:06 +0200

    Add few int types

---
+++ include/stl/int.hpp
@@ -0,0 +1,24 @@
+/*
+	xernel - x86_64 kernel written in C++20
+	Copyright (C) 2023  Jacquwes
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+typedef unsigned int uint32_t;
+typedef long unsigned int uint64_t;
+
+typedef uint64_t size_t;
